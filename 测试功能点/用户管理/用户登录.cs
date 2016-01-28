cea79d5c87bfa0cc8370f30d090f34ef4eb1bb27
cedarwy <cedarjeff@hotmail.com>
2016-01-28 16:51:39 +0800

    缺少的文件

---
+++ 测试功能点/用户管理/用户登录.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppuimTestTools.测试功能点
+{
+    public class 用户登录 : funPoint
+    {
+        public 用户登录(BaseClass b, string id):base(b, id)
+        {
+            //编写前置条件
+        }
+        public override void init(TestCaseStatus status)
+        {
+            //编写前置条件
+
+            base.init(status);
+        }
+        public override void Pass()
+        {
+            //base.Pass();
+        }
+        public override void Error()
+        {
+            //base.Error();
+        }
+    }
+}
