5289221e31a176f6d6a01bc831d9510a1eb97880
kongxincai <heshuo@langtianhealth.com>
2016-01-28 16:42:24 +0800

    555

--- 测试脚本/Demo脚本/demo03.cs
+++ 测试脚本/Demo脚本/demo03.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace AppuimTestTools.测试脚本.Demo脚本
 {
-    class demo03
+    public class demo03 : BaseClass
     {
+        public demo03()
+        {
+            Name = "测试03";
+            seqID = 3F;
+            Desc = "空心菜做的一个例子";
+        }
+        public override void Run()
+        {
+            //android.driver.CloseApp();
+
+        }
     }
 }
