0ab8234a7bf5481058c75cf3e11cf88f7e650747
kongxincai <heshuo@langtianhealth.com>
2016-02-01 15:03:12 +0800

    demo5

--- 测试脚本/Demo脚本/demo05.cs
+++ 测试脚本/Demo脚本/demo05.cs
@@ -23,6 +23,63 @@ namespace AppuimTestTools.测试脚本.Demo脚本
             Log("启动完成");
             android.sleep();
             
-        }
+            var a = android.driver.FindElementsById("com.lthealth.iwo:id/tab_btn_title");
+            foreach(var one in a)
+            {
+                if (one.Text == "我的")
+                {
+                    one.Click();
+
+                }
+            }
+            var z = android.driver.FindElementById("com.lthealth.iwo:id/my_info_name");
+            if (z.Text == "点击登录")
+
+            {
+                var b = android.driver.FindElementById("com.lthealth.iwo:id/my_info_name");
+
+                b.Click();
+                Log("点击登陆");
+                SaveScreenSnap("登陆页面");
+
+                var c = android.driver.FindElementById("com.lthealth.iwo:id/et_login_activity_username");
+                c.SendKeys("13699260115");
+                Log("输入用户名");
+                SaveScreenSnap("输入用户名");
+                android.sleep();
+                var d = android.driver.FindElementById("com.lthealth.iwo:id/edit_login_activity_password");
+                d.SendKeys("123456");
+                Log("输入密码");
+                SaveScreenSnap("输入密码");
+                android.sleep();
+                var e = android.driver.FindElementById("com.lthealth.iwo:id/btn_login_activity_login");
+                e.Click();
+                Log(" 点击登陆");
+                SaveScreenSnap("登陆完成");
+                android.sleep();
+            }
+
+            else
+            {
+                var x = android.driver.FindElementById("com.lthealth.iwo:id/my_info_setting");
+
+                x.Click();
+                Log("点击设置");
+                SaveScreenSnap("设置");
+
+                var y = android.driver.FindElementById("com.lthealth.iwo:id/btn_exit");
+
+                y.Click();
+                Log("退出登陆");
+                SaveScreenSnap("退出登陆");
+
+                var q = android.driver.FindElementById("com.lthealth.iwo:id/dialog_left_txt");
+                q.Click();
+                Log("确认退出");
+                SaveScreenSnap("确认退出");
+              }
+
+
+        }   
     }
 }
